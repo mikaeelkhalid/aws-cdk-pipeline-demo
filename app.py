15d0e5868b996b0a9d2d56fd246cbac097d0b5e0
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-12-30 16:10:52 +0500

    cleaning

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import os
 
 from aws_cdk import core as cdk
 
-# For consistency with TypeScript code, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
 
 from pipeline_demo.pipeline_demo_stack import PipelineDemoStack
