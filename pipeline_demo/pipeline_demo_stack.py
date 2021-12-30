1bb4791c1f4379bed9cc74dc3b249b4e00ec06c4
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-12-30 16:12:33 +0500

    cleaning

--- pipeline_demo/pipeline_demo_stack.py
+++ pipeline_demo/pipeline_demo_stack.py
@@ -5,25 +5,13 @@ from aws_cdk import (
     aws_apigateway as apigw,
 )
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
-
 
 class PipelineDemoStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PipelineDemoQueue",
-        #     visibility_timeout=cdk.Duration.seconds(300),
-        # )
         this_dir = path.dirname(__file__)
 
         handler = _lambda.Function(
